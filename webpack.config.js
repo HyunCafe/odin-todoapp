587251f642e61dcc97c9d056caa2832203e2cfec
HyunCafe <eparksuncafe@gmail.com>
2023-03-24 17:15:27 -0500

    Add: Add Date FNS external lib

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    todocreation: './src/todocreation.js'
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
