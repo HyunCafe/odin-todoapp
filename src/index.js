7d987abd91341c4a365d0483696f0109ac79c003
HyunCafe <eparksuncafe@gmail.com>
2023-03-24 17:16:03 -0500

    Add: Added basicl HTML Layout with BEM naming

--- src/index.js
+++ src/index.js
@@ -1,42 +1,49 @@
 
-
-// Game Plan
-
-// Break down the project into smaller tasks:
-// a. Create the structure for todo items using constructors or classes.
-// b. Implement project organization.
-// c. Separate application logic from DOM manipulation.
-// d. Design the User Interface.
-// e. Add localStorage for data persistence.
-
-// Set up your development environment:
-// a. Initialize a new project with npm and webpack.
-// b. Install necessary dependencies (e.g., date-fns).
-
-// Design the data structure:
-// a. Create a Todo class with properties like title, description, dueDate, priority, notes, and checklist.
-// b. Create a Project class that contains a list of todos and a project title.
-
-// Implement project organization:
-// a. Create methods to add, edit, and delete projects.
-// b. Allow users to select a project and view the associated todos.
-
-// Separate application logic from DOM manipulation:
-// a. Create a module for DOM manipulation functions (e.g., render projects and todos).
-// b. Create a module for application logic (e.g., adding, editing, or deleting todos).
-
-// Design the User Interface:
-// a. Create a layout similar to Trello, with columns for each project.
-// b. Display todos as cards within the columns.
-// c. Allow users to expand cards to see or edit details.
-// d. Implement drag-and-drop functionality for moving todos between projects or changing their order.
-
-// Implement localStorage for data persistence:
-// a. Write functions to save and load projects and todos in localStorage.
-// b. Call these functions when the app is loaded and when changes are made to the data.
-
-// Optimize and finalize:
-// a. Test the app thoroughly to ensure all features work as expected.
-// b. Refactor and optimize the code for readability and performance.
-// c. Deploy your project.
+const setupEventListeners = () => {
+    const navLinks = document.querySelectorAll("nav a");
+    navLinks.forEach((link) => {
+      link.addEventListener("click", (e) => {
+        e.preventDefault();
+        const content = document.querySelector("#content");
+        content.innerHTML = "";
+        switch (link.textContent.toLowerCase()) {
+          case "home":
+            content.append(createHeader());
+            content.append(createMain());
+            content.append(createFooter());
+            setupEventListeners();
+            break;
+          case "menu":
+            import("./modules/menu.js").then((module) => {
+              content.append(module.default());
+              setupEventListeners();
+            });
+            break;
+          case "about":
+            import("./modules/about.js").then((module) => {
+              content.append(module.default());
+              setupEventListeners();
+            });
+            break;
+          case "reservations":
+            import("./modules/reservations.js").then((module) => {
+              content.append(module.default());
+              setupEventListeners();
+            });
+            break;
+          case "contact":
+            import("./modules/contact.js").then((module) => {
+              content.append(module.default());
+              setupEventListeners();
+            });
+            break;
+          default:
+            break;
+        }
+      });
+    });
+  };
+  
+  setupEventListeners();
+  
 
