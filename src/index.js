4e6a89cb78963e4d382037e62a9d40e985f90bd6
HyunCafe <eparksuncafe@gmail.com>
2023-03-24 17:51:08 -0500

    Feat: Working on todocreation feature, working with class structure this time again

--- src/index.js
+++ src/index.js
@@ -1,50 +1,50 @@
 
-const setupEventListeners = () => {
-    const navLinks = document.querySelectorAll("nav a");
-    navLinks.forEach((link) => {
-      link.addEventListener("click", (e) => {
-        e.preventDefault();
-        const content = document.querySelector("#content");
-        content.innerHTML = "";
-        switch (link.textContent.toLowerCase()) {
-          case "home":
-            content.append(createHeader());
-            content.append(createMain());
-            content.append(createFooter());
-            setupEventListeners();
-            break;
-          case "menu":
-            import("./modules/menu.js").then((module) => {
-              content.append(module.default());
-              setupEventListeners();
-            });
-            break;
-          case "about":
-            import("./modules/about.js").then((module) => {
-              content.append(module.default());
-              setupEventListeners();
-            });
-            break;
-          case "reservations":
-            import("./modules/reservations.js").then((module) => {
-              content.append(module.default());
-              setupEventListeners();
-            });
-            break;
-          case "contact":
-            import("./modules/contact.js").then((module) => {
-              content.append(module.default());
-              setupEventListeners();
-            });
-            break;
-          default:
-            break;
-        }
-      });
-    });
-  };
+// const setupEventListeners = () => {
+//     const navLinks = document.querySelectorAll("nav a");
+//     navLinks.forEach((link) => {
+//       link.addEventListener("click", (e) => {
+//         e.preventDefault();
+//         const content = document.querySelector("#content");
+//         content.innerHTML = "";
+//         switch (link.textContent.toLowerCase()) {
+//           case "home":
+//             content.append(createHeader());
+//             content.append(createMain());
+//             content.append(createFooter());
+//             setupEventListeners();
+//             break;
+//           case "menu":
+//             import("./modules/menu.js").then((module) => {
+//               content.append(module.default());
+//               setupEventListeners();
+//             });
+//             break;
+//           case "about":
+//             import("./modules/about.js").then((module) => {
+//               content.append(module.default());
+//               setupEventListeners();
+//             });
+//             break;
+//           case "reservations":
+//             import("./modules/reservations.js").then((module) => {
+//               content.append(module.default());
+//               setupEventListeners();
+//             });
+//             break;
+//           case "contact":
+//             import("./modules/contact.js").then((module) => {
+//               content.append(module.default());
+//               setupEventListeners();
+//             });
+//             break;
+//           default:
+//             break;
+//         }
+//       });
+//     });
+//   };
   
-  setupEventListeners();
+//   setupEventListeners();
   
 
 // Modify Above to fit into current project
