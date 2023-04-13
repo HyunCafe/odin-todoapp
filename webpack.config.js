5502dc4ee9c9c6733956a1174a1329f9ffe2b3d2
HyunCafe <eparksuncafe@gmail.com>
2023-04-13 13:28:33 -0500

    Doc: Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  mode: 'development',
+  mode: isDevelopment ? 'development' : 'production',
   entry: './src/index.js',
   output: {
     filename: '[name].bundle.js',
